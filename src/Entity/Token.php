6ec9512703c3f8be621aadf1924fc6ede80ffe64
j20hook <jgarciamartin@safareyes.es>
2024-01-12 20:55:21 +0100

    cambios de modelo

--- src/Entity/Token.php
+++ src/Entity/Token.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TokenRepository::class)]
+#[ORM\Table(name: "token", schema: "safatuber24")]
 class Token
 {
     #[ORM\Id]
