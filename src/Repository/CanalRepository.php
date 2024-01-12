488530f31bd06cfe2bac5dc93e97920bb11eecda
robee05 <rbullonperez@safareyes.es>
2024-01-12 16:20:56 +0100

    creacion de modelo canal, usuario, enum tipo contenido

---
+++ src/Repository/CanalRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Canal;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Canal>
+ *
+ * @method Canal|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Canal|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Canal[]    findAll()
+ * @method Canal[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CanalRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Canal::class);
+    }
+
+//    /**
+//     * @return Canal[] Returns an array of Canal objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Canal
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
