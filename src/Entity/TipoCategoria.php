7eda80ce55f043253f77f5f780a56dc835cb6a5b
j20hook <jgarciamartin@safareyes.es>
2024-01-15 18:44:06 +0100

    dos tipos mas

--- src/Entity/TipoCategoria.php
+++ src/Entity/TipoCategoria.php
@@ -6,6 +6,7 @@ use App\Repository\TipoCategoriaRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TipoCategoriaRepository::class)]
+#[ORM\Table(name: "tipo_categoria", schema: "safatuber24")]
 class TipoCategoria
 {
     #[ORM\Id]
