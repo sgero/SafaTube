5c642a3821b75801eb8ce658e6979100da39e39e
sgarcia <sgarciaguerrero@safareyes.es>
2024-02-20 13:23:43 +0100

    tareas acometidas

--- src/Controller/DiscordController.php
+++ src/Controller/DiscordController.php
@@ -22,15 +22,16 @@ class DiscordController extends AbstractController
 }
 
 
-//TODO: falta en BBDD crear el campo Webhook con la URL de cada usuario
+//TODO: falta en BBDD crear el campo Webhook con la URL de cada usuario ***********DONE
 
-//TODO: falta hacer el metodo para que cuando uno se registra te envie el mail de validacion con el token, y te redirija a la pantalla de validacion de la cuenta, en la que tendrás que meter el mail y el token
+//TODO: falta hacer el metodo para que cuando uno se registra te envie el mail de validacion con el token, y te redirija a la pantalla de validacion de la cuenta, en la que tendrás que meter el mail y el token******DONE
 
 //TODO: si coincide saldra un toaster con usuario verificado con exito y te redirigira a la pantalla de login. (SWEETER ALERT)
 
-//TODO: los usuarios verificados si podrán loguearse, los no verificados no.
+//TODO: los usuarios verificados si podrán loguearse, los no verificados no.**********DONE
 
-//TODO: los canales automaticamente se pondrán como verificados y si podran ser listados en la pantalla de canales.
+//TODO: los canales automaticamente se pondrán como verificados y si podran ser listados en la pantalla de canales.************DONE
+
 
 //TODO: RECUPERAR CONTRASEÑA. para recuperar contraseña se introducirà el email del usuario, y hará una llamada del usuario y su token de verificación se enviará por webhook. Acto seguido se abrirá una pantalla para introducir el token y el nombre de usuario asociada al mail, introduciendo ese token de verificacion junto al nombre de usuario, se abrira un modal para cambiar la contraseña.
 
